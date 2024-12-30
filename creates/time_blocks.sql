4003354e4a00fc3c5861d4bda7a1178455b7fcba
tenghoit <tenghoitkouch+github@gmail.com>
2024-12-30 04:37:03 +0000

    added time conflict function

--- creates/time_blocks.sql
+++ creates/time_blocks.sql
@@ -8,3 +8,25 @@ CREATE VIEW time_blocks_view AS
 SELECT  start_time,
         end_time
 FROM time_blocks;
+
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS find_time_conflict$$
+CREATE FUNCTION find_time_conflict(
+    start_time_1 TIME,
+    end_time_1 TIME,
+    start_time_2 TIME,
+    end_time_2 TIME
+)   
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    RETURN (
+        ((start_time_1 >= start_time_2) AND (start_time_1 <= end_time_2))
+        OR
+        ((end_time_1 >= start_time_2) AND (end_time_1 <= end_time_2))
+        OR
+        ((start_time_1 <= start_time_2) AND (end_time_1 >= end_time_2))
+    );
+END$$
+DELIMITER ;
