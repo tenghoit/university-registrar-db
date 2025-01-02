1a0cfe689361d10e7c9c22531fafce07edea66ef
tenghoit <tenghoitkouch+github@gmail.com>
2025-01-02 02:38:42 +0000

    added student_degrees

---
+++ creates/student_degrees.sql
@@ -0,0 +1,20 @@
+CREATE TABLE student_degrees (
+    student_id  INT,
+    degree_id   INT,
+    PRIMARY KEY (student_id, degree_id),
+    FOREIGN KEY (student_id) REFERENCES students(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (degree_id) REFERENCES degrees(degree_id) ON DELETE RESTRICT,
+);
+
+CREATE VIEW student_degrees_view AS
+SELECT  student_id
+        student_first_name,
+        student_last_name,
+        degree_id,
+        degree_name,
+        degree_type
+FROM    student_degrees
+        JOIN students_view
+        USING(student_id)
+        JOIN degrees_view
+        USING(degree_id);
