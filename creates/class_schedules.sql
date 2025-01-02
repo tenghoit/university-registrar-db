e4037a03fa119bd3eb5f9a65a197f0ec6bc08fee
tenghoit <tenghoitkouch+github@gmail.com>
2025-01-02 02:30:42 +0000

    added location and professor conflict

--- creates/class_schedules.sql
+++ creates/class_schedules.sql
@@ -27,14 +27,118 @@ FROM    class_schedules
 GROUP BY    class_id;
 
 
--- DROP FUNCTION IF EXISTS find_class_schedule_conflict
--- CREATE FUNCTION find_class_schedule_conflict(class_schedule_id_one_input INT, class_schedule_id_two_input INT)
--- RETURNS INT
--- RETURN (
--- SELECT COUNT(*)
--- FROM    class_schedule_conflicts_view
--- WHERE   cs1.class_schedule_id = class_schedule_id_one_input
---         AND cs2.class_schedule_id = class_schedule_id_two_input
--- );
+CREATE VIEW classes_with_schedule_view AS
+SELECT  class_id, 
+        course_id
+        section,
+        term_id
+        professor_id,
+        building_name,
+        room_number,
+        class_max_capacity,
+        day_letter,
+        start_time,
+        end_time
+FROM    classes
+        JOIN class_schedules
+        USING (class_id);
 
 
+DROP FUNCTION IF EXISTS find_location_conflicts;
+CREATE FUNCTION find_location_conflicts(
+    building_name_input VARCHAR(64), 
+    room_number_input VARCHAR(64), 
+    term_id_input INT,
+    day_letter_input VARCHAR(1),
+    start_time_input TIME, 
+    end_time_input TIME)
+RETURNS INT
+RETURN (
+    SELECT  COUNT(class_id)
+    FROM    classes_with_schedule_view
+    WHERE   building_name = building_name_input
+            AND room_number = room_number_input
+            AND term_id = term_id_input
+            AND day_letter = day_letter_input
+            AND find_time_conflict(start_time, end_time, start_time_input, end_time_input,) <> 0          
+);
+
+
+DROP FUNCTION IF EXISTS find_professor_conflicts;
+CREATE FUNCTION find_professor_conflicts(
+    professor_id_input INT, 
+    term_id_input INT,
+    day_letter_input VARCHAR(1),
+    start_time_input TIME, 
+    end_time_input TIME)
+RETURNS INT
+RETURN (
+    SELECT  COUNT(class_id)
+    FROM    classes_with_schedule_view
+    WHERE   professor_id = professor_id_input
+            AND term_id = term_id_input
+            AND day_letter = day_letter_input
+            AND find_time_conflict(start_time, end_time, start_time_input, end_time_input,) <> 0          
+);
+
+
+
+DELIMITER $$
+CREATE TRIGGER class_schedules_insert
+BEFORE INSERT ON class_schedules FOR EACH ROW
+BEGIN
+
+    -- location_conflict
+    SET @location_conflicts = find_location_conflicts(  get_building_name_by_class(NEW.class_id), 
+                                                        get_room_number_by_class(NEW.class_id), 
+                                                        get_term_id_by_class(NEW.class_id), 
+                                                        NEW.day_letter,
+                                                        NEW.start_time, 
+                                                        NEW.end_time);
+    IF (@location_conflicts <> 0) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Class already exists at that location on that time';
+    END IF;
+
+    -- professor conflict
+        SET @professor_conflicts = find_professor_conflicts(get_professor_id_by_class(NEW.class_id), 
+                                                            get_term_id_by_class(NEW.class_id), 
+                                                            NEW.day_letter,
+                                                            NEW.start_time, 
+                                                            NEW.end_time);
+    IF (@professor_conflicts <> 0) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Professor is already teaching a class on that time';
+    END IF;
+
+END; $$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE TRIGGER class_schedules_update
+BEFORE UPDATE ON class_schedules FOR EACH ROW
+BEGIN
+
+    -- location_conflict
+    SET @location_conflicts = find_location_conflicts(  get_building_name_by_class(NEW.class_id), 
+                                                        get_room_number_by_class(NEW.class_id), 
+                                                        get_term_id_by_class(NEW.class_id), 
+                                                        NEW.day_letter,
+                                                        NEW.start_time, 
+                                                        NEW.end_time);
+    IF (@location_conflicts <> 0) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Class already exists at that location on that time';
+    END IF;
+
+    -- professor conflict
+        SET @professor_conflicts = find_professor_conflicts(get_professor_id_by_class(NEW.class_id), 
+                                                            get_term_id_by_class(NEW.class_id), 
+                                                            NEW.day_letter,
+                                                            NEW.start_time, 
+                                                            NEW.end_time);
+    IF (@professor_conflicts <> 0) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Professor is already teaching a class on that time';
+    END IF;
+
+END; $$
+DELIMITER ;
--- creates/classes.sql
+++ creates/classes.sql
@@ -3,86 +3,84 @@ CREATE TABLE classes (
     course_id           INT,
     term_id             INT,
     building_name       VARCHAR(64),
-    room_number         INT,
+    room_number         VARCHAR(64),
     professor_id        INT,
     section             VARCHAR(1) DEFAULT 'a',
     class_max_capacity  INT DEFAULT 25,
-    class_schedule_id   INT,
-    start_time          TIME,
-    end_time            TIME,
     PRIMARY KEY (class_id),
     FOREIGN KEY (course_id) REFERENCES courses (course_id) ON DELETE RESTRICT,
     FOREIGN KEY (term_id) REFERENCES terms (term_id) ON DELETE RESTRICT,
     FOREIGN KEY (building_name, room_number) REFERENCES locations (building_name, room_number) ON DELETE SET NULL,
     FOREIGN KEY (professor_id) REFERENCES professors (professor_id) ON DELETE RESTRICT,
-    FOREIGN KEY (class_schedule_id) REFERENCES class_schedules (class_schedule_id) ON DELETE RESTRICT,
-    FOREIGN KEY (start_time, end_time) REFERENCES time_blocks (start_time, end_time) ON DELETE RESTRICT,
-
-    CONSTRAINT unique_class UNIQUE (course_id, term_id, section) -- b/c these needs to be unique
+    UNIQUE (course_id, term_id, section)
 );
 
 
 CREATE VIEW classes_view AS
-SELECT  class_id, 
-        course_id
-        crs.course_discipline AS course_discipline,
-        crs.course_number AS course_number,
-        cls.section AS section,
-        crs.course_name AS course_name,
-        professor_first_name,
-        professor_last_name,
-        building_name,
-        room_number,
-        class_schedule,
-        start_time,
-        end_time,
-        term_start_date,
-        term_end_date,
-        class_max_capacity,
-        cpr.prerequisites AS prerequisites
-        FROM classes AS cls
-        JOIN terms
-            USING (term_id)
-        JOIN professors
-            USING (professor_id)
-        JOIN class_schedules_view
-            USING (class_schedule_id)
-        JOIN courses as crs
-            ON cls.course_id = crs.course_id
-        LEFT OUTER JOIN course_prerequisites_view AS cpr
-            ON cls.course_id = cpr.course_id
-GROUP BY    class_id
+SELECT      class_id, 
+            course_id
+            course_discipline,
+            course_number,
+            section,
+            course_name,
+            term_name
+            professor_first_name,
+            professor_last_name,
+            building_name,
+            room_number,
+            class_max_capacity,
+            prerequisites
+FROM        classes
+            JOIN courses_view
+                USING (course_id)
+            JOIN terms_view
+                USING (term_id)
+            JOIN professors_view
+                USING (professor_id)
+            LEFT OUTER JOIN course_prerequisites_single_view AS cpsv
+                ON course_id = cpsv.primary_course.course_id
 ORDER BY    term_start_date DESC,
             course_discipline ASC,
             course_number ASC,
             section ASC;
 
+
+DROP FUNCTION IF EXISTS get_building_name_by_class;
+CREATE FUNCTION get_building_name_by_class(class_id_input INT)
+RETURNS VARCHAR(64)
+RETURN (
+    SELECT  building_name
+    FROM    classes
+    WHERE   class_id = class_id_input
+);
+
+DROP FUNCTION IF EXISTS get_room_number_by_class;
+CREATE FUNCTION get_room_number_by_class(class_id_input INT)
+RETURNS VARCHAR(64)
+RETURN (
+    SELECT  room_number
+    FROM    classes
+    WHERE   class_id = class_id_input
+);
+
 DROP FUNCTION IF EXISTS get_term_id_by_class;
 CREATE FUNCTION get_term_id_by_class(class_id_input INT)
 RETURNS INT
 RETURN (
-    SELECT term_id
+    SELECT  term_id
     FROM    classes
     WHERE   class_id = class_id_input
 );
 
-DROP FUNCTION IF EXISTS get_start_time_by_class;
-CREATE FUNCTION get_start_time_by_class(class_id_input INT)
+DROP FUNCTION IF EXISTS get_professor_id_by_class;
+CREATE FUNCTION get_professor_id_by_class(class_id_input INT)
 RETURNS INT
 RETURN (
-    SELECT  start_time
+    SELECT  professor_id
     FROM    classes
     WHERE   class_id = class_id_input
 );
 
-DROP FUNCTION IF EXISTS get_end_time_by_class;
-CREATE FUNCTION get_end_time_by_class(class_id_input INT)
-RETURNS INT
-RETURN (
-    SELECT end_time
-    FROM    classes
-    WHERE   class_id = class_id_input
-);
 
 DROP FUNCTION IF EXISTS get_class_max_capacity;
 CREATE FUNCTION get_class_max_capacity(class_id_input INT)
@@ -92,112 +90,3 @@ RETURN (
     FROM    classes
     WHERE   class_id = class_id_input
 );
-
-
-DROP FUNCTION IF EXISTS has_location_conflict;
-CREATE FUNCTION has_location_conflict(
-    building_name_input VARCHAR(64), 
-    room_number_input INT, 
-    term_id_input INT,
-    class_schedule_id_input INT,
-    start_time_input TIME, 
-    end_time_input TIME)
-RETURNS INT
-RETURN (
-    SELECT COUNT(class_id)
-        FROM    classes
-        WHERE   building_name = building_name_input
-                AND room_number = room_number_input
-                AND term_id = term_id_input
-                AND find_class_schedule_conflict(class_schedule_id_input, class_schedule_id) <> 0
-                AND (
-                    ((start_time >= start_time_input) AND (start_time <= end_time_input))
-                    OR
-                    ((end_time >= start_time_input) AND (end_time <= end_time_input))
-                )             
-);
-
-DROP FUNCTION IF EXISTS has_professor_conflict;
-CREATE FUNCTION has_professor_conflict(
-    professor_id_input INT, 
-    term_id_input INT,
-    class_schedule_id_input INT,
-    start_time_input TIME, 
-    end_time_input TIME)
-RETURNS INT
-RETURN (
-    SELECT COUNT(class_id)
-        FROM    classes
-        WHERE   professor_id = professor_id_input
-                AND term_id = term_id_input
-                AND find_class_schedule_conflict(class_schedule_id_input, class_schedule_id) <> 0
-                AND (
-                    ((start_time >= start_time_input) AND (start_time <= end_time_input))
-                    OR
-                    ((end_time >= start_time_input) AND (end_time <= end_time_input))
-                )             
-);
-
- 
-DELIMITER $$
-CREATE TRIGGER classes_insert
-BEFORE INSERT ON classes FOR EACH ROW
-BEGIN
-
-    -- location conflict
-    SET @location_conflicts = has_location_conflict(NEW.building_name, 
-                                                    NEW.room_number, 
-                                                    NEW.term_id, 
-                                                    NEW.class_schedule_id,
-                                                    NEW.start_time, 
-                                                    NEW.end_time);
-    IF (@location_conflicts <> 0) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Class already exists at that location on that time';
-    END IF;
-
-    -- professor conflict
-    SET @professor_conflicts = has_professor_conflict(  NEW.professor_id, 
-                                                        NEW.term_id, 
-                                                        NEW.class_schedule_id,
-                                                        NEW.start_time, 
-                                                        NEW.end_time);
-    IF (@professor_conflicts <> 0) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Professor is already teaching a class on that time';
-    END IF;
-
-END; $$
-DELIMITER ;
-
-
-DELIMITER $$
-CREATE TRIGGER classes_update
-BEFORE UPDATE ON classes FOR EACH ROW
-BEGIN
-
-    -- location conflict
-    IF (NEW.building_name <> OLD.building_name) OR (NEW.room_number <> OLD.room_number) THEN -- check if changed first
-        SET @location_conflicts = has_location_conflict(NEW.building_name, 
-                                                        NEW.room_number, 
-                                                        NEW.term_id, 
-                                                        NEW.class_schedule_id,
-                                                        NEW.start_time, 
-                                                        NEW.end_time);
-        IF (@location_conflicts <> 0) THEN
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Class already exists at that location on that time';
-        END IF;
-    END IF;
-
-    -- professor conflict
-    IF (NEW.professor_id <> OLD.professor_id) THEN -- check if changed first
-        SET @professor_conflicts = has_professor_conflict(  NEW.professor_id, 
-                                                            NEW.term_id, 
-                                                            NEW.class_schedule_id,
-                                                            NEW.start_time, 
-                                                            NEW.end_time);
-        IF (@professor_conflicts <> 0) THEN
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Professor is already teaching a class on that time';
-        END IF;
-    END IF;
-
-END; $$
-DELIMITER ;
