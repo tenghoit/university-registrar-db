81c0bd9be112b5cbdfb62bd480ffde8d0bb5921f
tenghoit <tenghoitkouch+github@gmail.com>
2025-01-05 18:29:51 +0000

    class size conflict done

--- creates/class_schedules.sql
+++ creates/class_schedules.sql
@@ -26,18 +26,48 @@ FROM    class_schedules
         USING (class_id);
 
 
+-- CREATE VIEW class_schedules_single_view AS
+-- SELECT  class_id,
+--         GROUP_CONCAT(
+--             DISTINCT CONCAT(
+--                 day_letter,
+--                 ' ',
+--                 DATE_FORMAT(start_time, '%l:%i%p'),  -- Format the start time (e.g., 1:00pm)
+--                 '-',
+--                 DATE_FORMAT(end_time, '%l:%i%p')   -- Format the end time (e.g., 2:00pm)
+--             )
+--             ORDER BY FIELD(day_letter, 'M', 'T', 'W', 'R', 'F', 'S', 'U'), start_time
+--             SEPARATOR ', '
+--         ) AS schedule
+-- FROM    class_schedules_view
+-- GROUP BY    class_id;
+
+
+CREATE VIEW grouped_schedules AS
+SELECT  class_id,
+        start_time,
+        end_time,
+        GROUP_CONCAT(
+            DISTINCT day_letter
+            ORDER BY FIELD(day_letter, 'M', 'T', 'W', 'R', 'F', 'S', 'U')
+            SEPARATOR ''
+        ) AS grouped_days
+FROM    class_schedules_view
+GROUP BY    class_id, 
+            start_time, 
+            end_time;
+
 CREATE VIEW class_schedules_single_view AS
 SELECT  class_id,
         GROUP_CONCAT(
             DISTINCT CONCAT(
-                day_letter,
-                ' ',
-                DATE_FORMAT(start_time, '%l:%i%p'),  -- Format the start time (e.g., 1:00pm)
+                grouped_days, ' ',
+                DATE_FORMAT(start_time, '%l:%i%p'),  -- Format the start time (e.g., 8:00 AM)
                 '-',
-                DATE_FORMAT(end_time, '%l:%i%p')   -- Format the end time (e.g., 2:00pm)
+                DATE_FORMAT(end_time, '%l:%i%p')    -- Format the end time (e.g., 9:00 AM)
             )
-            ORDER BY FIELD(day_letter, 'M', 'T', 'W', 'R', 'F', 'S', 'U'), start_time
+            ORDER BY FIELD(grouped_days, 'M', 'T', 'W', 'R', 'F', 'S', 'U'), start_time
             SEPARATOR ', '
         ) AS schedule
-FROM    class_schedules_view
+FROM    grouped_schedules
 GROUP BY    class_id;
--- tests/class_size_conflict.sql
+++ tests/class_size_conflict.sql
@@ -8,9 +8,23 @@ INSERT INTO classes (
     class_max_capacity
 )
 VALUES
-    (1, 1, 'OLIN', '101', 1, 'a', 1); -- 1
+    (1, 1, 'OLIN', '101', 1, 'a', 2); -- 1
 
 SELECT * FROM classes_view;
+
+INSERT INTO class_schedules(
+    class_id,
+    day_letter,
+    start_time,
+    end_time
+)
+VALUES
+    (1, 'M', '08:00:00', '09:00:00'),
+    (1, 'W', '08:00:00', '09:00:00'),
+    (1, 'F', '08:00:00', '09:00:00'),
+    (1, 'T', '09:10:00', '10:10:00');
+
+SELECT * FROM class_schedules_view;
 
 INSERT INTO student_class_history(
     student_id,
