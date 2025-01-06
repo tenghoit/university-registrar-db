28454d66bd02fe03d618821ff0f335fd5e94de96
tenghoit <tenghoitkouch+github@gmail.com>
2025-01-06 03:29:46 +0000

    PDO success

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,25 @@
 <?php
 
-$config = parse_ini_file('/home/pi/configs/mysql.ini');
-$host = $config['mysqli.default_host'];
-$dbname = 'university_registrar';
-$dsn = "mysql:host={$host};dbname={$dbname}";
-
 try {
+    $config = parse_ini_file('../configs/mysql.ini');
+    $host = $config['mysqli.default_host'];
+    $dbname = 'university_registrar';
+    $dsn = "mysql:host={$host};dbname={$dbname}";
+
+    echo $config['mysqli.default_host'];
+    echo $config['mysqli.default_user'];
+    echo $config['mysqli.default_pw'];
+
     $pdo = new PDO(
         $dsn,
         $config['mysqli.default_user'],
         $config['mysqli.default_pw']
     );
+
+    echo $config['mysqli.default_host'];
+    echo $config['mysqli.default_user'];
+    echo $config['mysqli.default_pw'];
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-if ($_SERVER != 'POST') {
-    header("Location: ../index.php");
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header("Location: ../php/courses.php");
     exit();
 }
 
@@ -15,14 +15,14 @@ try {
     require_once "dbh.inc.php";
     $query = file_get_contents("../queries/courses_insert.sql");
     $stmt = $pdo->prepare($query);
+
+    $stmt->bindParam(":course_discipline", $course_discipline);
+    $stmt->bindParam(":course_number", $course_number);
+    $stmt->bindParam(":course_name", $course_name);
+    $stmt->bindParam(":course_credits", $course_credits);
+    $stmt->bindParam(":course_description", $course_description);
     
-    $stmt->execute([
-        $course_discipline, 
-        $course_number, 
-        $course_name, 
-        $course_credits, 
-        $course_description
-    ]);
+    $stmt->execute();
 
     $pdo = null;
     $stmt = null;
